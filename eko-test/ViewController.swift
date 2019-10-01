7e9ffe49175c98415531f8a616b61f8dfb6e32b9
Nishan <nrlnishan@gmail.com>
2019-10-01 21:52:12 +0545

    Initialize Project

---
+++ eko-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eko-test
+//
+//  Created by Nishan Niraula on 10/1/19.
+//  Copyright © 2019 nishan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
